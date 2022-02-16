349c95be7804a2491900409e0c4ef37f249341c9
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-02-16 12:45:31 +0100

    create initial list component

---
+++ client/components/List/List.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import type {teaming} from "../../../types";
+
+interface ListProps {
+    id: teaming.ListId;
+}
+
+function List({id}: ListProps) {
+  return (
+    <div className="List">
+    </div>
+  );
+}
+
+export default List;
