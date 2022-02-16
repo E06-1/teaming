fa4106101d940492210cb4261ebc0c0dae4a6f98
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-02-16 12:45:04 +0100

    create store and app hooks

---
+++ client/redux/store.ts
@@ -0,0 +1,28 @@
+import { configureStore, Middleware } from '@reduxjs/toolkit'
+import boardSlice from './features/boardSlice'
+import cardSlice from './features/cardSlice'
+import listSlice from './features/listSlice'
+
+const testMiddleware: Middleware = () => {
+  return next => action => {
+    console.warn("IMPLEMENT ME! This middleware should forward the received Action to the Backend via Websocket.");
+    console.log('will dispatch', action)
+
+  // Call the next dispatch method in the middleware chain.
+  return next(action)
+  }
+}
+
+export const store = configureStore({
+  reducer: {
+    list: listSlice,
+    card: cardSlice,
+    board: boardSlice
+  },
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(testMiddleware)
+})
+
+// Infer the `RootState` and `AppDispatch` types from the store itself
+export type RootState = ReturnType<typeof store.getState>
+// Inferred type: {posts: PostsState, comments: CommentsState, users: UsersState}
+export type AppDispatch = typeof store.dispatch
