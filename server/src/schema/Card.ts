e3be40d1101e9328cddbcb0389812e156fff2e8a
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-02-18 00:55:12 +0100

    Add _id:String to Schemas

--- server/src/schema/Card.ts
+++ server/src/schema/Card.ts
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 
 const cardSchema = new mongoose.Schema({
-    
+    _id: String, //https://mongoosejs.com/docs/guide.html#_id ---> const schema = new Schema({ _id: Number }); should work, so i guess String as well.
     content:{
         type: String,
         required:false
