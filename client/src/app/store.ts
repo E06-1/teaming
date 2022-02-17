e39669fccdd87694406ccd96b3b4eef6a6d157b5
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-02-17 16:30:46 +0100

    Add local Storage Save and forward Action Middleware

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -3,7 +3,9 @@ import boardSlice from '../features/board/boardSlice'
 import cardSlice from '../features/card/cardSlice'
 import listSlice from '../features/list/listSlice'
 
-const testMiddleware: Middleware = () => {
+
+//Middleware should forward the action via Websocket Connection to the Backend, needs to be implemented
+const forwardAction: Middleware = () => {
   return next => action => {
     console.warn("IMPLEMENT ME! This middleware should forward the received Action to the Backend via Websocket.");
     console.log('will dispatch', action)
@@ -13,14 +15,22 @@ const testMiddleware: Middleware = () => {
   }
 }
 
+
+
 export const store = configureStore({
   reducer: {
     list: listSlice,
     card: cardSlice,
     board: boardSlice
   },
-  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(testMiddleware)
+  middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(forwardAction)
 })
+
+const saveToLocalStorage = async () => {
+  localStorage.setItem("state", JSON.stringify(store.getState())) 
+}
+
+store.subscribe(saveToLocalStorage)
 
 // Infer the `RootState` and `AppDispatch` types from the store itself
 export type RootState = ReturnType<typeof store.getState>
