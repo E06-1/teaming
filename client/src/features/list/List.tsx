09478f807dee17f541de5b6c6f90b04e29469087
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-02-17 16:31:08 +0100

    Update List Component

--- client/src/features/list/List.tsx
+++ client/src/features/list/List.tsx
@@ -1,14 +1,58 @@
-import React from 'react';
-import type {teaming} from "../../../../types";
+import {
+  Box,
+  List as MuiList,
+  ListItem,
+  ListItemButton,
+  ListSubheader,
+  Paper,
+  Card,
+} from "@mui/material";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import Menu from "@mui/icons-material/Menu";
+import Add from "@mui/icons-material/Add";
+import React from "react";
+import { v4 } from "uuid";
+import type { teaming } from "../../../../types";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import { createCard } from "../card/cardSlice";
+import { addCard, selectList } from "./listSlice";
 
 interface ListProps {
-    id: teaming.ListId;
+  id: teaming.ListId;
 }
 
-function List({id}: ListProps) {
+function List({ id }: ListProps) {
+  const dispatch = useAppDispatch();
+  const { cards, header } = useAppSelector(selectList(id));
+
+  const handleCreateCard = () => {
+    const cardId: teaming.CardId = `card:${v4()}`;
+    dispatch(createCard({ cardId, content: "new content" }));
+    dispatch(addCard({ listId: id, cardId }));
+  };
   return (
-    <div className="List">
-    </div>
+    <Paper elevation={1} className="List">
+      <MuiList>
+        <ListItem sx={{ display: "flex", justifyContent: "space-between" }}>
+          <ListSubheader>{header}</ListSubheader>
+          <Menu />
+        </ListItem>
+
+        {cards.map((id) => (
+          <ListItem key={id}>
+            <Card>{id}</Card>
+          </ListItem>
+        ))}
+        <ListItem>
+          <ListItemButton onClick={handleCreateCard}>
+            <ListItemIcon>
+              <Add />
+            </ListItemIcon>{" "}
+            Add Card
+          </ListItemButton>
+        </ListItem>
+      </MuiList>
+    </Paper>
   );
 }
 
