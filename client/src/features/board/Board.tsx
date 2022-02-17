ba2ab1cca50ab1d3cc7f47cb80285da94834401e
Kevin Kötz <8590427+KevinKoetz@users.noreply.github.com>
2022-02-17 16:31:00 +0100

    Update Board Component

--- client/src/features/board/Board.tsx
+++ client/src/features/board/Board.tsx
@@ -1,13 +1,37 @@
-import React from 'react';
-import type {teaming} from "../../../../types";
+import { Container } from "@mui/material";
+import React from "react";
+import { v4 } from "uuid";
+import type { teaming } from "../../../../types";
+import { useAppDispatch, useAppSelector } from "../../app/hooks";
+import List from "../list/List";
+import { createList } from "../list/listSlice";
+import { addList, selectBoard } from "./boardSlice";
 
 interface BoardProps {
-    id: teaming.BoardId;
+  id: teaming.BoardId;
 }
 
-function Board({id}: BoardProps) {
+function Board({ id }: BoardProps) {
+  const dispatch = useAppDispatch();
+  const { name, lists, collaborators, admins } = useAppSelector(
+    selectBoard(id)
+  );
+
+  const handleCreateList = () => {
+    const listId: teaming.ListId = `list:${v4()}`;
+    dispatch(createList({ listId, header: "new" }));
+    dispatch(addList({ boardId: id, listId }));
+  };
+
   return (
     <div className="Board">
+      <h2>{name}</h2>
+      <Container sx={{display: "flex"}}>    
+        {lists.map((id) => (
+          <List key={id} id={id} />
+        ))}
+        <input type="button" value="Create List" onClick={handleCreateList} />
+      </Container>
     </div>
   );
 }
