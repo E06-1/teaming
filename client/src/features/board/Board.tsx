a025c999e4c44f73c62c10144f8835e8779373cb
TanyaT <tanyatodorova5@gmail.com>
2022-02-23 14:24:45 +0100

    Impleanting iamges as background

--- client/src/features/board/Board.tsx
+++ client/src/features/board/Board.tsx
@@ -1,5 +1,6 @@
 import Box from "@mui/material/Box";
 import Container from "@mui/material/Container";
+import { useEffect } from "react";
 import { useDrop } from "react-dnd";
 import { v4 } from "uuid";
 import type { teaming } from "../../../../types";
@@ -7,7 +8,8 @@ import { useAppDispatch, useAppSelector } from "../../app/hooks";
 import List from "../list/List";
 import { createList } from "../list/listsSlice";
 import { selectBoard, selectListIdsOnBoard } from "./boardsSlice";
-
+import { useState } from "react";
+import axios from "axios";
 interface BoardProps {
   id: teaming.BoardId;
 }
@@ -25,10 +27,38 @@ function Board({ id }: BoardProps) {
   const [, drop] = useDrop({
     accept: "list",
   });
+  const [image, setImage] = useState("");
+  useEffect(() => {
+    axios.get("/image/random").then((res) => {
+      console.log(res.data);
+      setImage(res.data);
+    });
+  }, []);
 
   return (
-    <Box sx={{ backgroundColor: "primary.main" }} className="Board" ref={drop}>
+    <Box
+      sx={{ backgroundImage: image, position: "relative" }}
+      className="Board"
+      ref={drop}
+    >
       <h2>{name}</h2>
+      {image ? (
+        <img
+          style={{
+            position: "absolute",
+            top: "0",
+            right: "0",
+            left: "0",
+            bottom: "0",
+            zIndex: "-1",
+            objectFit: "cover",
+            display: "block",
+            width:"100%",
+            height:"100%"
+          }} 
+          src={image}
+        />
+      ) : null}
       <Container sx={{ display: "flex", gap: "1rem" }}>
         {lists.map((id) => (
           <List key={id} id={id} />
